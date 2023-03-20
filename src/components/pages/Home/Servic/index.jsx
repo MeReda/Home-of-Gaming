86a88c3d75a3ce3c45140c9cf11d17c7339178ce
C H A S K Y_41 <red.layne18@gmail.com>
2023-03-20 17:37:19 +0100

    added vid

---
+++ src/components/pages/Home/Servic/index.jsx
@@ -0,0 +1,167 @@
+const Servic = () => {
+    return(
+        <section class="services" id="services">
+    <div class="container">
+
+      <div class="main-txt">
+        <h1><span>J</span>ouer à tous les meilleurs jeux multi-joueus du moment</h1>
+      </div>
+
+      <div class="row" style="margin-top: 30px;">
+
+
+
+
+        <div class="col-lg-3 py-3 py-md-0">
+          <div class="card-body">
+
+
+
+            <img src="./games-images/Apex.svg" width="300" alt="">
+
+          </div>
+        </div>
+        <div class="col-lg-3 py-3 py-md-0">
+          <div class="card-body">
+            <img src="./games-images/BUBG.svg" width="300" alt="">
+          </div>
+        </div>
+        <div class="col-lg-3 py-3 py-md-0">
+          <div class="card-body">
+            <img src="./games-images/CS.svg" width="300" alt="">
+          </div>
+        </div>
+
+        <div class="col-lg-3 py-3 py-md-0">
+          <div class="card-body">
+            <img src="./games-images/Fortnite.svg" width="300" alt="">
+          </div>
+        </div>
+
+
+        <div class="row" style="margin-top: 30px;">
+
+
+
+
+          <div class="col-lg-3 py-3 py-md-0">
+            <div class="card-body">
+
+
+
+              <img src="./games-images//Rocket League.svg" width="300" alt=""/>
+
+            </div>
+          </div>
+          <div class="col-lg-3 py-3 py-md-0">
+            <div class="card-body">
+              <img src="./games-images/Rainbow 6 Siege.svg" width="300" alt=""/>
+            </div>
+          </div>
+          <div class="col-lg-3 py-3 py-md-0">
+            <div class="card-body">
+              <img src="./games-images/Rectangle 17.svg" width="300" alt=""/>
+            </div>
+          </div>
+
+          <div class="col-lg-3 py-3 py-md-0">
+            <div class="card-body">
+              <img src="./games-images/Rectangle 18.svg" width="300" alt=""/>
+            </div>
+          </div>
+
+
+
+          <div class="row" style="margin-top: 30px;">
+
+
+
+
+            <div class="col-lg-3 py-3 py-md-0">
+              <div class="card-body">
+
+
+
+                <img src="./games-images/Rectangle 19.svg" width="300" alt=""/>
+
+              </div>
+            </div>
+            <div class="col-lg-3 py-3 py-md-0">
+              <div class="card-body">
+                <img src="./games-images//Rectangle 21.svg" width="300" alt=""/>
+              </div>
+            </div>
+            <div class="col-lg-3 py-3 py-md-0">
+              <div class="card-body">
+                <img src="./games-images/Rectangle 22.svg" width="300" alt=""/>
+              </div>
+            </div>
+
+            <div class="col-lg-3 py-3 py-md-0"/>
+              <div class="card-body ">
+                <div class="bg">
+                   <h3>ET PLEINS DE <br> NOUVEAUTéS <br> PROCHAINEMENT</h3>
+                </div>
+               
+               
+              </div>
+            </div>
+
+
+
+          </div>
+
+
+          <div class="row" style="margin-top: 30px;"/>
+
+            <div class="col-md-4 py-3 py-md-0">
+
+              <div class="card">
+                <i class="fas fa-globe-asia"></i>
+                <div class="card-body">
+                  <h3>IN The Game</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, eaque.</p>
+                </div>
+              </div>
+
+            </div>
+            <div class="col-md-4 py-3 py-md-0"/>
+
+              <div class="card">
+                <i class="fas fa-plane"></i>
+                <div class="card-body">
+                  <h3>About</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, eaque.</p>
+                </div>
+              </div>
+
+            </div>
+            <div class="col-md-4 py-3 py-md-0">
+
+              <div class="card">
+                <i class="fas fa-hiking"></i>
+                <div class="card-body">
+                  <h3>VESION</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, eaque.</p>
+                </div>
+              </div>
+
+            </div>
+
+
+
+          </div>
+
+        </div>
+  </section>
+        
+
+
+
+
+
+
+
+    )
+}
+export default Servic ;
