cfc145aa9606dd4ba2deb81425f44a2e1d47eab1
imad <arabiimad@outlook.com>
2023-03-31 21:37:24 +0000

    logo-nav-footer

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from "/img/logo.png";
+import Logo from "/img/logo.svg";
 import './index.scss'; // import the SCSS file
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'; // import Bootstrap components
 import {FaInstagram } from "react-icons/fa";
--- src/components/Layout/NavBar/index.jsx
+++ src/components/Layout/NavBar/index.jsx
@@ -1,4 +1,4 @@
-import Logo from "/img/logo.png";
+import Logo from "/img/logo.svg";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
